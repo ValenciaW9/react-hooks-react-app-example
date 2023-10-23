e0f66a7e2b35656b9ba1f10ea70343c1067708d1
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-22 21:40:51 -0400

    react-hooks-react-app-example/
    react-hooks-react-app-example/

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,27 +3,60 @@ import { render, screen } from "@testing-library/react";
 import { format } from "date-fns";
 import App from "../components/App";
 
-beforeEach(() => {
-  render(<App />);
-});
-
 test('should include "Now" in the header instead of a time', () => {
-  expect(
-    screen.queryByText(format(new Date(), "MMMM do yyyy, h:mm:ss a"))
-  ).not.toBeInTheDocument();
-  expect(screen.queryByText(/Now/g)).toBeInTheDocument();
+  render(<App />);
+  expect(screen.queryByText(format(new Date(), "MMMM do yyyy, h:mm:ss a"))).not.toBeInTheDocument();
+  expect(screen.getByText(/Now/g)).toBeInTheDocument();
 });
 
 test("should include the <ExampleComponent />", () => {
-  expect(screen.queryByText("Whoa!")).toBeInTheDocument();
+  render(<App />);
+  expect(screen.getByText("Whoa!")).toBeInTheDocument();
 });
 
 test("should include the <TestComponent />", () => {
-  expect(screen.queryByTitle("time video")).toBeInTheDocument();
+  render(<App />);
+  expect(screen.getByTitle("time video")).toBeInTheDocument();
 });
 
-//   it('should include "Now" in the header instead of a time', () => {
-//     expect(wrapper.find('header').text()).to.not.include(moment().format('MMMM Do YYYY'))
+
+test('should include "Now" in the header instead of a time', () => {
+  render(
+    <App>
+      <ExampleComponent />
+      <TestComponent />
+    </App>
+  );
+  expect(screen.queryByText(format(new Date(), "MMMM do yyyy, h:mm:ss a"))).not.toBeInTheDocument();
+  expect(screen.getByText(/Now/g)).toBeInTheDocument();
+});
+
+test("should include the <ExampleComponent />", () => {
+  render(
+    <App>
+      <ExampleComponent />
+      <TestComponent />
+    </App>
+  );
+  expect(screen.getByText("Whoa!")).toBeInTheDocument();
+});
+
+test("should include the <TestComponent />", () => {
+  render(
+    <App>
+      <ExampleComponent />
+      <TestComponent />
+    </App>
+  );
+  expect(screen.getByTitle("time video")).toBeInTheDocument();
+});
+
+
+
+
+
+
+
 //     expect(wrapper.find('header').text()).to.include('Now')
 //   });
 
@@ -35,3 +68,4 @@ test("should include the <TestComponent />", () => {
 //     expect(wrapper.text()).to.include('<TestComponent />')
 //   });
 // });
+
